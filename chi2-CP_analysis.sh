b84cf696facedc0d5951ff7d89738b8aa1197874
useakat@kekcc <useakat@gmail.com>
2016-10-21 16:11:42 +0900

    modify chi2-CP routines: separately treat SK, HK1, HK2

--- chi2-CP_analysis.sh
+++ chi2-CP_analysis.sh
@@ -15,43 +15,15 @@ FV_HK=$6 # kton
 FV_Kr=$7 # kton
 POT=$8 # * 10^21 POT
 
-#mares=1100
-
 CPmode=CP
 fitMH=true
 
 exp=2
-#L=1040
 OAB_SK=2.5
 rho_SK=2.6
 SL=295
-#OAB_far=2.3
-#rho_far=2.9
 
-#run_name=T2HKK_C_chi2-CP_2.7_187
 ./makedir.sh rslt_$run_name 1
-
-SV=`echo "scale=3; $FV_HK + 22.5" | bc`
-
-./set_param_mode.sh 0 "iSK" 1
-./set_param_mode.sh 0 "SV" $SV
-./set_param_mode.sh 0 "SL" $SL
-./set_param_mode.sh 0 "Srho" $rho_SK
-./set_param_mode.sh 0 "SOAB" $OAB_SK
-
-./set_param_mode.sh 0 "iOKi" 0
-./set_param_mode.sh 0 "KV" 100
-./set_param_mode.sh 0 "OL" 695
-./set_param_mode.sh 0 "Orho" 2.75
-./set_param_mode.sh 0 "OOAB" 0.9
-
-./set_param_mode.sh 0 "iKr" $iKr
-./set_param_mode.sh 0 "KV" $FV_Kr
-./set_param_mode.sh 0 "KL" $KL
-./set_param_mode.sh 0 "Krho" $rho_Kr
-./set_param_mode.sh 0 "KOAB" $OAB_Kr
-
-./set_param_mode.sh 0 "Y" $POT
 
 ./set_param.sh "thatm" 0.5
 ./set_param.sh "fthatm" 0.5
--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 
 ./set_params_card.sh
 
+sed -e "s/scan_mode=0/scan_mode=1/" chi2_scan.sh > tmp.sh
+#sed -e "s/scan_mode=1/scan_mode=0/" chi2_scan.sh > tmp.sh
+mv tmp.sh chi2_scan.sh
+chmod +x chi2_scan.sh
+
 POT=2.7 # * 10^21
 
 ###### site C
@@ -42,11 +47,27 @@ dir=rslt_T2HKK_H_${FV_Kr}kt_${POT}POT
 # mv rslt_T2HKK_H_chi2-th23_${FV_Kr}kt_${POT}POT $dir/chi2-th23
 
 ####### HK*2
-iKr=0
-FV_HK=374
-#POT=2.7 # * 10^21
+./set_param_mode.sh 0 "iSK" 1
+./set_param_mode.sh 0 "SV" 22.5
+./set_param_mode.sh 0 "SL" 295
+./set_param_mode.sh 0 "Srho" 2.6
+./set_param_mode.sh 0 "SOAB" 2.5
 
-# dummy inputs
+./set_param_mode.sh 0 "iOki" 1 # for HK1
+./set_param_mode.sh 0 "OV" 187
+./set_param_mode.sh 0 "OL" 295
+./set_param_mode.sh 0 "Orho" 2.6
+./set_param_mode.sh 0 "OOAB" 2.5
+
+./set_param_mode.sh 0 "iKr" 1 # for HK2
+./set_param_mode.sh 0 "KV" 187
+./set_param_mode.sh 0 "KL" 295
+./set_param_mode.sh 0 "Krho" 2.6
+./set_param_mode.sh 0 "KOAB" 2.5
+./set_param_mode.sh 0 "Y" $POT
+
+FV_HK=374
+iKr=1  # for HK2
 KL=295
 OAB_Kr=2.5
 FV_Kr=187
@@ -55,9 +76,9 @@ rho_Kr=2.6
 dir=rslt_T2HKK_HK_${FV_HK}kt_${POT}POT
 ./makedir.sh $dir 1
 
-./MH_analysis.sh T2HKK_HK_MH_${FV_HK}kt_${POT}POT $iKr $KL $OAB_Kr $rho_Kr $FV_HK $FV_Kr $POT
-mv rslt_T2HKK_HK_MH_${FV_HK}kt_${POT}POT $dir/MH
-./chi2-CP_analysis.sh T2HKK_HK_chi2-CP_${FV_HK}kt_${POT}POT $iKr $KL $OAB_Kr $rho_Kr $FV_HK $FV_Kr $POT
-mv rslt_T2HKK_HK_chi2-CP_${FV_HK}kt_${POT}POT $dir/chi2-CP
+#./MH_analysis.sh T2HKK_HK_MH_${FV_HK}kt_${POT}POT $iKr $KL $OAB_Kr $rho_Kr $FV_HK $FV_Kr $POT
+#mv rslt_T2HKK_HK_MH_${FV_HK}kt_${POT}POT $dir/MH
+#./chi2-CP_analysis.sh T2HKK_HK_chi2-CP_${FV_HK}kt_${POT}POT $iKr $KL $OAB_Kr $rho_Kr $FV_HK $FV_Kr $POT
+#mv rslt_T2HKK_HK_chi2-CP_${FV_HK}kt_${POT}POT $dir/chi2-CP
 ./chi2-th23_analysis.sh T2HKK_HK_chi2-th23_${FV_HK}kt_${POT}POT $iKr $KL $OAB_Kr $rho_Kr $FV_HK $FV_Kr $POT
 mv rslt_T2HKK_HK_chi2-th23_${FV_HK}kt_${POT}POT $dir/chi2-th23
