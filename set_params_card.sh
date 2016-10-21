fba00f56a4f3240fa32ea30eee46f45d819592c6
useakat@kekcc <useakat@gmail.com>
2016-10-21 16:10:51 +0900

    add set_params_card.sh

---
+++ set_params_card.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#params_card=params.card_2016.09
+#params_card=params.card_2016.09_nosmear
+params_card=params.card_2016.09_nosmear_nofit
+#params_card=params.card_new_50MeV  # template of parameter card for T2KK/T2KO study (1605.02368)
+cp -rf temp/$params_card temp/params.card 
